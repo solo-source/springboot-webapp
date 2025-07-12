d96075cfd8d7a70b4ab6db608220f473767832bd
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2025-07-12 10:03:29 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/stackit/webapp/StackItApplication.java
@@ -0,0 +1,13 @@
+package com.stackit.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackItApplication.class, args);
+	}
+
+}
